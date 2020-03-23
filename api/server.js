8a68728db73a8234550a816a315ed9ac2bfe3ae9
Gremlin4544 <alve@mckethen.com>
2020-03-23 18:36:21 -0400

    moved migrations folder to data folder - server now working

--- api/server.js
+++ api/server.js
@@ -1,13 +1,33 @@
 const express = require("express");
 const helmet = require("helmet");
 const cors = require("cors");
-// const session = require('express-session');
+const session = require('express-session');
+
+const usersRouter = require("../users/users-router.js");
+const authRouter = require('../auth/router.js');
+const restricted = require("../auth/restricted-middleware.js");
 
 const server = express();
+
+const sessionConfig = {
+  name: 'alveRules', //default name is sid - always rename
+  secret: 'keep it secret, keep it safe!',
+  cookie: {
+    maxAge: 1000 * 60,
+    secure: false, //false in dvlpmt but will be true in production to send only over https
+    httpOnly: true, //true means that JS in the browser or JS anywhere, will not have access
+  },
+  resave: false,
+  saveUninitialized: true, //GDPR laws require you to ask the client - cannot set cookies to save automatically
+};
 
 server.use(helmet());
 server.use(express.json());
 server.use(cors());
+server.use(session(sessionConfig));
+
+server.use("/api/users", restricted, usersRouter);
+server.use("/api/auth", authRouter)
 
 server.use((err, req, res, next) => {
     console.log(err);
--- users/users-router.js
+++ users/users-router.js
@@ -3,6 +3,7 @@ const router = require("express").Router();
 const Users = require("./users-model.js");
 
 router.get("/", (req, res) => {
+  console.log("Qui Gon - its working! Its working!");
   Users.find()
     .then(users => {
       res.json(users);
